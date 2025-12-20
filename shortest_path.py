f3a8e8137925fa30f35a8bddc9084bf84724f33c
Playdovsky <144317216+Playdovsky@users.noreply.github.com>
2025-12-20 12:52:59 +0100

    Graph visualization
    Graph visualization created with graphviz + pydot. Be aware, configuration of graphviz may be tricky and requires specific environmental path to be provided for OS (graphviz bin path)

--- shortest_path.py
+++ shortest_path.py
@@ -1,4 +1,8 @@
 import heapq
+import os
+import pydot
+graphviz_path = r'C:\Program Files\Graphviz\bin' 
+os.environ["PATH"] += os.pathsep + graphviz_path
 
 class ShortestPath:
     def __init__(self, vertices, num_of_edges, start_vertex, end_vertex, edges):
@@ -9,11 +13,12 @@ class ShortestPath:
         self.graph_edges = edges
         
         self.find_path(edges)
+        self.visualize_graph()
 
     def find_path(self, edges):
-        final_weights_sum = 0
-        final_edges_sum = 0
-        final_path = []
+        self.final_weights_sum = 0
+        self.final_edges_sum = 0
+        self.final_path = None
         self.pq = []
 
         for edge in edges:
@@ -25,9 +30,9 @@ class ShortestPath:
             current_vertex = current_path[-1]
 
             if current_vertex == self.end_vertex:
-                final_weights_sum = current_weight
-                final_path = current_path
-                final_edges_sum = len(current_path) - 1
+                self.final_weights_sum = current_weight
+                self.final_path = current_path
+                self.final_edges_sum = len(current_path) - 1
                 break
 
             for edge in self.graph_edges:
@@ -36,8 +41,27 @@ class ShortestPath:
                     new_path = current_path + [edge[1]]
                     heapq.heappush(self.pq, (new_weight, new_path))
         
-        if final_edges_sum == 0:
+        if not self.final_path:
             print(f"No path found from ({self.start_vertex}) to ({self.end_vertex})")
             return
         
-        print(f"for the shortest path algorithm travels through {final_edges_sum} edges\nwith total sum of {final_weights_sum} weights\n({') --> ('.join(final_path)})")
+        print(f"for the shortest path algorithm travels through {self.final_edges_sum} edges\nwith total sum of {self.final_weights_sum} weights\n({') --> ('.join(self.final_path)})")
+
+    def visualize_graph(self):
+        graph = pydot.Dot(graph_type='digraph', rankdir='LR')
+        
+        for vertex in self.vertices:
+            node = pydot.Node(str(vertex))
+            graph.add_node(node)
+        
+        for edge in self.graph_edges:
+            graph.add_edge(pydot.Edge(str(edge[0]), str(edge[1]), label=str(edge[2])))
+        
+        if self.final_path:
+            for i in range(len(self.final_path) - 1):
+                start = str(self.final_path[i])
+                end = str(self.final_path[i + 1])
+                graph.add_edge(pydot.Edge(start, end, color='red', penwidth='3'))
+        
+        graph.write_png('graph_visualization.png')
+        print("Graph visualization saved to graph_visualization.png")
